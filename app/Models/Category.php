4ac0f9d176d260c2726a3ef824d2820ef81ec3fa
Arsal <arsalkamoka786@gmail.com>
2025-04-24 16:07:50 +0500

    changes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,7 +10,7 @@ class Category extends Model
     use HasFactory;
 
 
-    protected $fillable  = ['name','parent_id','is_popular','status','price'];
+    protected $fillable  = ['name','parent_id','is_popular','status','price','image'];
 
 
     protected $hidden = [
